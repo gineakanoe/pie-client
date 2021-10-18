bcdf0baa68d609d9463c9443030288cbdde74f7d
Ginea Merrill <gineakanoe@gmail.com>
2021-10-18 15:36:18 -0400

    added all pages

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+const [sessionToken, setSessionToken] = useState("undefined");
+
+  return (
+    <div className="App">
+      <h1>This is a test</h1>
+      {sessionToken}
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
