74baa236ac80048a69129c1adb52eed9bd371226
Ginea Merrill <gineakanoe@gmail.com>
2021-10-19 16:19:59 -0400

    added info to components and connected stuff

---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,90 @@
+import React, {useState} from 'react';
+import './auth.css';
+
+const Auth = (props) => {
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLasttName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [login, setLogin] = useState(true);
+
+    const title = () => {
+        return !login ? 'Signup' : 'Login'
+    };
+
+    const loginToggle = (e) => {
+        e.preventDefault();
+
+        setLogin(!login)
+
+        setEmail('');
+        setPassword('');
+        setFirstName('');
+        setLasttName('');
+    }
+
+    const signupFields = () => !login ? (
+        <div>
+            <label htmlFor='fistName'>First Name:</label>
+            <br/>
+            <input type='type' id='firstName' value={firstName} onChange={(e) => setFirstName(e.target.value)} />  
+            <br/>
+            <label htmlFor='lastName'>Last Name:</label>
+            <br/>
+            <input type='text' id='lastName' value={lastName} onChange={(e) => setLasttName(e.target.value)} />
+        </div>
+    ) : null;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        let reqBody = login ? {
+            email: email,
+            password:password
+        } : {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password
+        } 
+
+        let url = login ? 'http://localhost:4000/user/login' : 'http://localhost:4000/user/register';
+
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(reqBody),
+            headers: new Headers({
+                'Content-Type' : 'application/json'
+            })
+        }) 
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(err => console.log(err))
+
+
+    }
+
+    return (
+        <div>
+            <form>
+                <button type='button' onClick={loginToggle}>Login / Signup Toggle</button>
+                <br/>
+                <h1>{title()}</h1>
+                {signupFields()}
+                <label htmlFor='email'>Email:</label>
+                <br/>
+                <input type='type' id='email' value={email} onChange={(e) => setEmail(e.target.value)} />  
+                <br/>
+                <label htmlFor='password'>Password:</label>
+                <br/>
+                <input type='password' id='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                <br/>
+                <button type='submit' onClick={handleSubmit}>Submit</button>
+            </form>
+
+        </div>
+    )
+}
+
+export default Auth;
---
+++ src/components/Pies/Pies.js
@@ -0,0 +1,40 @@
+//* Set up a boilerplate for a component called Pies. Include a css file of the same name.
+//* Silver: Add a table within the return. The head will include: Name of Pie, Base of Pie, Crust, Bake Time, Servings, Rating. The body of the table will be empty.
+//* Gold: import useState from react. Create a state variable of pies that has a default value of an empty array.
+
+import React, {useState} from 'react';
+import './pies.css';
+
+const Pies = (props) => {
+
+const [pies, setPies] = useState([]);
+
+    // const [nameOfPie, setNameOfPie] = useState('');
+    // const [baseOfPie, setBaseOfPie] = useState('');
+    // const [crust, setCrust] = useState('');
+    // const [bakeTime, setBakeTime] = useState('');
+    // const [servings, setServings] = useState('');
+    // const [rating, setrating] = useState('');
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name of Pie</th>
+                    <th>Base of Pie</th>
+                    <th>Crust</th>
+                    <th>Bake Time</th>
+                    <th>Servings</th>
+                    <th>Rating</th>
+                </tr>
+            </thead>
+            <tbody>
+                
+            </tbody>
+
+        </table>
+    )
+
+};
+
+export default Pies;
